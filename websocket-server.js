81e26068201c1f669cfc21b09c8dba9b139314e6
Sundiata2 <kevinlbelanger@gmail.com>
2018-01-18 14:15:56 -0500

    Get rid of logs, some listing and function comments

--- websocket-server.js
+++ websocket-server.js
@@ -9,21 +9,16 @@ module.exports = function startServer(io) {
       socket.emit('update', "You have connected to the server");
       io.emit('update', name + ' has joined the server');
       io.emit('update-people', people);
-      console.log(name + ' has joined the server');
-      console.log('current users: ' + people);
     });
 
     socket.on('send', msg => {
       io.emit('chat', people[socket.id], msg);
-      console.log(people[socket.id] + ' has sent a message: ' + msg);
     });
 
     socket.on('disconnect', () => {
       io.emit('update', people[socket.id] + ' has left the server');
-      console.log(people[socket.id] + ' has left the server');
       delete people[socket.id];
       io.emit('update-people', people);
-      console.log('current users: ' + people);
     });
   });
 }
